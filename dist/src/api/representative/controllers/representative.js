e327bd19b7a2eaecde2a6ff4cd0bfa265820abbf
greatlions <aslan.abylkas@gmail.com>
2024-04-29 21:58:09 +0500

    Add files via upload

---
+++ dist/src/api/representative/controllers/representative.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * representative controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::representative.representative');
